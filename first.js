0bfb5878866ca91f6882ea73ff0c2abfe366d0b1
@abhishek.mohan <abhishek.mohan@surboard.se>
2022-08-04 16:39:31 +0530

    classes & interfaces

--- first.js
+++ first.js
@@ -1,4 +1,11 @@
 "use strict";
+var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (receiver, state, value, kind, f) {
+    if (kind === "m") throw new TypeError("Private method is not writable");
+    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a setter");
+    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot write private member to an object whose class did not declare it");
+    return (kind === "a" ? f.call(receiver, value) : f ? f.value = value : state.set(receiver, value)), value;
+};
+var _Employee_id;
 //number
 let age;
 age = 22;
@@ -47,6 +54,61 @@ function addr(a, b, ...c) {
     return a + b + c.reduce((acc, num) => acc + num);
 }
 let numbers = [1, 2, 3];
+//**************************************************************
+//Classes
+//calling without constructor
+/*
+class Employee {
+    id!: number;
+
+    name!: string;
+
+    address!: string;
+}
+
+let abhishek = new Employee();
+
+abhishek.id = 1;
+abhishek.name = "Abhishek Mohan";
+abhishek.address = "Chennai"
+
+console.log(abhishek)
+*/
+//calling with constructor
+class Employee {
+    constructor(id, name, address) {
+        _Employee_id.set(this, void 0);
+        __classPrivateFieldSet(this, _Employee_id, id, "f");
+        this.name = name;
+        this.address = address;
+    }
+    getAddress() {
+        return `${this.name} stays in ${this.address}`;
+    }
+}
+_Employee_id = new WeakMap();
+let abhishek = new Employee(1, "Abhishek Mohan A", "G.S.T Road");
+let lynn = new Employee(2, "Lynn", "Vikings");
+let addressing = abhishek.getAddress();
+console.log(abhishek);
+console.log(lynn);
+console.log(addressing);
+// # -> for private
+//Inheritance
+//Creating a derived class
+class Manager extends Employee {
+    constructor(id, name, address) {
+        super(id, name, address);
+    }
+    getManager() {
+        return `${this.name} is a manager at ${this.address}.`;
+    }
+}
+let mark = new Manager(1, "Mark Doe", "Michigan");
+console.log(mark.getManager());
+console.log(mark.getAddress());
+/*
 console.log(addr(2, 3, ...numbers));
 console.log(sub(4, 1));
 console.log(mul(4, 1));
+*/
--- first.ts
+++ first.ts
@@ -1,30 +1,30 @@
 //number
-let age : number;
+let age: number;
 age = 22;
 
 //array
-let numlist : Array<number>;
-numlist = [1,2,3,3,4]
+let numlist: Array<number>;
+numlist = [1, 2, 3, 3, 4];
 
-let sum = numlist.reduce((acc, num) => acc + num)
+let sum = numlist.reduce((acc, num) => acc + num);
 
-let result = numlist.filter((num)=> num > 2 )
+let result = numlist.filter((num) => num > 2);
 
-let empList : Array<string>
-empList = ["Abh","Ash","lynn","Syd"]
-let rollOut = empList.find((emp) => emp == "Abh")
+let empList: Array<string>;
+empList = ["Abh", "Ash", "lynn", "Syd"];
+let rollOut = empList.find((emp) => emp == "Abh");
 
 //string
-let  student;
-student = "Abhishek"
+let student;
+student = "Abhishek";
 
 //enum
 const enum Color {
-    Red,
-    Blue,
-    Green
+	Red,
+	Blue,
+	Green,
 }
-let c:Color = Color.Blue;
+let c: Color = Color.Blue;
 
 // let swapnumbers: [number,number]
 
@@ -36,40 +36,128 @@ let c:Color = Color.Blue;
 // swapnumbers[0];
 
 //any
-let department : any;
-department = "Asset"
+let department: any;
+department = "Asset";
 department = "10";
 
 //functions
 //way one
-function add(a:number,b:number):number{
-    return a+b;
+function add(a: number, b: number): number {
+	return a + b;
 }
 
 //way two
-const sub = (a:number,b:number):number =>  a-b;
+const sub = (a: number, b: number): number => a - b;
 
-const mul = (a:number,b:number):number =>{
-     return a*b 
+const mul = (a: number, b: number): number => {
+	return a * b;
 };
 
 //optional parameter
-function adds(a:number,b:number,c?:number):number{
-    return a+b;
+function adds(a: number, b: number, c?: number): number {
+	return a + b;
 }
 
 //required parameter
-function addq(a:number,b:number, c=10):number{
-    return a+b+c;
+function addq(a: number, b: number, c = 10): number {
+	return a + b + c;
 }
 
 //rest parameters
-function addr(a:number,b:number,...c:number[]):number{
-    return a+b+c.reduce(( acc, num) => acc + num);
+function addr(a: number, b: number, ...c: number[]): number {
+	return a + b + c.reduce((acc, num) => acc + num);
 }
 
-let numbers = [1,2,3]
+let numbers = [1, 2, 3];
 
-console.log(addr(2,3,...numbers))
-console.log(sub(4,1))
-console.log(mul(4,1))
+
+//**************************************************************
+//Classes
+//calling without constructor
+/*
+class Employee {
+	id!: number;
+
+	name!: string;
+
+	address!: string;
+}
+
+let abhishek = new Employee();
+
+abhishek.id = 1;
+abhishek.name = "Abhishek Mohan";
+abhishek.address = "Chennai"
+
+console.log(abhishek)
+*/
+
+//calling with constructor
+ class Employee {
+     #id!: number;
+	 //if protected we can mention lke
+	 // protected name!: string;
+     name!: string;
+     address!: string;
+
+	 constructor(id: number, name: string, address: string){
+	   this.#id  = id;
+	   this.name = name;
+	   this.address = address;
+	 }
+
+	 getAddress() : string{
+     	return `${this.name} stays in ${this.address}`;
+     }
+ }
+
+let abhishek = new Employee(1,"Abhishek Mohan A","G.S.T Road");
+let lynn  = new Employee(2,"Lynn","Vikings");
+
+let addressing = abhishek.getAddress();
+
+console.log(abhishek);
+console.log(lynn);
+console.log(addressing)
+
+// # -> for private
+//Inheritance
+//Creating a derived class
+
+class Manager extends Employee{
+	constructor(id:number,name: string,address: string){
+		super(id,name,address);
+	}
+
+	getManager () :string{
+		return `${this.name} is a manager at ${this.address}.`;
+	}
+}
+
+let mark = new Manager(1,"Mark Doe","Michigan");
+console.log(mark.getManager())
+console.log(mark.getAddress()); //caling the function in parent class
+
+//Interfaces ************************************************************
+
+interface User{
+	name: string;
+	id: number;
+	sge: number;
+	sddress: string;
+}
+
+let user:User{ name:"John", id:1, address:"Chennai"}
+
+interface Employees extends User{
+	email:string;
+    salary:number;
+}
+
+let employee: Employees {name;"John",age:31,address:'Chennai',salary:20000,email:"jaohn@comoany.com"}
+	
+/*
+console.log(addr(2, 3, ...numbers));
+console.log(sub(4, 1));
+console.log(mul(4, 1));
+*/
